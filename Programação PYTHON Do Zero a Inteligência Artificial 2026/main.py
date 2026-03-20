b3546962860004d9c2cd3c251813ab842ef7175a
Fat01 <fat01@friovel.local>
2026-03-20 18:26:08 -0300

    iniciando python

---
+++ Programação PYTHON Do Zero a Inteligência Artificial 2026/main.py
@@ -0,0 +1,12 @@
+#preco = int(input("preço: "))
+#desconto = int(input("desconto: "))
+
+#novo_preco = preco - (preco * desconto / 100)
+
+#print(f'O valor com desconto é R$ {novo_preco}')
+
+produto = 10
+dias = int(input("quantos dias voce usa: "))
+porcao = int(input("quantas porções voce usa por dia "))
+
+print(produto - (produto - (porcao * dias)))
